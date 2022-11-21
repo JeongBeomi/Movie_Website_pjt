b30401817ef202eac33d0d2b79c9361e733f39e8
ssg-js <likemath1997@gmail.com>
2022-11-21 17:11:04 +0900

    [F006] 스타일 적용 완료, 텍스트 안보이기 만들기

--- final-pjt-front/src/views/MovieDetailView.vue
+++ final-pjt-front/src/views/MovieDetailView.vue
@@ -42,35 +42,5 @@ export default {
 
 <style>
 
-.css-5brfx4 {
-  display: none;
-  position: fixed;
-  top: 0px;
-  left: 0px;
-  z-index: 51;
-  background: transparent;
-  color: rgb(255, 255, 255);
-  text-align: center;
-  width: 100%;
-  height: 62px;
-  transition: background-color 200ms ease 0s;
-}
 
-
-.css-6k8tqb {
-  display:none;
-  position:fixed;
-  top:0px;
-  left:0;
-  z-index:51;
-  background:#fff;
-  color:#fff;
-  text-align:center;
-  width:100%;
-  height:62px;
-  box-shadow:0 1px 0 0 rgba(0,0,0,0.08);
-  -webkit-transition:background-color 200ms;
-  transition:background-color 200ms;
-  }
-  /* @media (min-width:720px){.css-6k8tqb{display:block;}} */
 </style>
