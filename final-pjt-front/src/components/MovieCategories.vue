036906afca9fab8ad7208c5984565717f8c3b430
ssg-js <likemath1997@gmail.com>
2022-11-22 00:37:12 +0900

    [F006] categories 카드 정렬 중

--- final-pjt-front/src/components/MovieCategories.vue
+++ final-pjt-front/src/components/MovieCategories.vue
@@ -1,20 +1,30 @@
 <template>
   <div>
-    <div class="movieMenu">
-      <b-button @click="orderedBy($event)" value="release_date">최신순</b-button>
-      <b-button @click="orderedBy($event)" value="vote_average">평점순</b-button>
+    <div>
+      <b-button @click="orderedBy($event)" value="release_date"
+        >최신순</b-button
+      >
+      <b-button @click="orderedBy($event)" value="vote_average"
+        >평점순</b-button
+      >
       <b-button @click="orderedBy($event)" value="popularity">인기순</b-button>
       <b-dropdown class="mx-0" right text="Genre">
-        <b-dropdown-item @click="clickedGenre($event)" id="28">action</b-dropdown-item>
-        <b-dropdown-item @click="clickedGenre($event)" id="12">adventure</b-dropdown-item>
-        <b-dropdown-item @click="clickedGenre($event)" id="16">animation</b-dropdown-item>
+        <b-dropdown-item @click="clickedGenre($event)" id="28"
+          >action</b-dropdown-item
+        >
+        <b-dropdown-item @click="clickedGenre($event)" id="12"
+          >adventure</b-dropdown-item
+        >
+        <b-dropdown-item @click="clickedGenre($event)" id="16"
+          >animation</b-dropdown-item
+        >
       </b-dropdown>
     </div>
-    
-    <b-container style="width: 80em">
+    <!-- movie -->
+    <b-container class="my-4">
       <b-row>
-        <b-col cols="4" v-for="movie in movies" :key="movie.id">
-          <MovieCategoriesItem :movie="movie"/>
+        <b-col class="my-1" cols="3" v-for="movie in movies" :key="movie.id">
+          <MovieCategoriesItem :movie="movie" />
         </b-col>
       </b-row>
     </b-container>
@@ -22,39 +32,35 @@
 </template>
 
 <script>
-import MovieCategoriesItem from '@/components/MovieCategoriesItem'
+import MovieCategoriesItem from "@/components/MovieCategoriesItem";
 
 export default {
-  name: 'MovieCategories',
+  name: "MovieCategories",
   components: {
     MovieCategoriesItem,
   },
   computed: {
     movies() {
-      return this.$store.state.allMovieList
-    }
+      return this.$store.state.allMovieList;
+    },
   },
   methods: {
     getGenreMovies(genre) {
-      this.$store.dispatch("getGenreMovies", genre)
+      this.$store.dispatch("getGenreMovies", genre);
     },
     clickedGenre(event) {
-      this.getGenreMovies(event.target.id)
+      this.getGenreMovies(event.target.id);
     },
     orderedBy(event) {
-      this.$store.dispatch("orderedBy", event.target.value)
-    }
+      this.$store.dispatch("orderedBy", event.target.value);
+    },
   },
-  created() {
-
-  }
-}
+  created() {},
+};
 </script>
 
 <style>
-.movieMenu {
+.left {
   text-align: left;
-  padding: 0px 0px 0px 10px;
 }
-
 </style>
